added Dataset for sem_seg, fixes bug of calculation nwrong metrics for small batches in training, added visualization draft for o3d, started working on test_semseg

data_utils/S3DISDataLoader.py
@@ -43,8 +43,8 @@ class S3DISDataset(Dataset):
 
     def __getitem__(self, idx):
         room_idx = self.room_idxs[idx]
-        points = self.room_points[room_idx]   # N * 6
-        labels = self.room_labels[room_idx]   # N
+        points = self.room_points[room_idx]  # N * 6
+        labels = self.room_labels[room_idx]  # N
         N_points = points.shape[0]
 
         while (True):
@@ -77,6 +77,55 @@ class S3DISDataset(Dataset):
 
     def __len__(self):
         return len(self.room_idxs)
+
+
+def pc_normalize(pc):
+    centroid = np.mean(pc, axis=0)
+    pc = pc - centroid
+    m = np.max(np.sqrt(np.sum(pc ** 2, axis=1)))
+    pc = pc / m
+    return pc
+
+
+class PartCustomDataset(Dataset):
+    def __init__(self, root='./data/custom_partseg_data', npoints=4096, split='train', class_choice=None, normal_channel=False):
+        self.npoints = npoints
+        self.root = root
+        self.point_clouds_dir = os.path.join(self.root, "point_clouds")
+        self.seg_dir = os.path.join(self.root, "labels")
+        self.normal_channel = normal_channel
+
+        self.ids = np.loadtxt(os.path.join(self.root, split + '.txt')).astype(np.int32)
+        self.labelweights = np.array([ 1., 1., 1.])
+        # todo augmentation
+        foo = 0
+
+    def __getitem__(self, index):
+        point_cloud_path = os.path.join(self.point_clouds_dir, str(self.ids[index]) + ".txt")
+        point_cloud = np.loadtxt(point_cloud_path, delimiter=",").astype(np.float32)
+        if not self.normal_channel:
+            point_set = point_cloud[:, 0:3]
+        else:
+            point_set = point_cloud[:, 0:6]
+
+        seg_path = os.path.join(self.seg_dir, str(self.ids[index]) + ".txt")
+        seg = np.loadtxt(seg_path).astype(np.int32)-1
+
+        point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
+        # TODO maybe normalize 3:6 too? if the colors will be>1
+
+        choice = np.random.choice(len(seg), self.npoints, replace=True)
+        # resample
+        point_set = point_set[choice, :]
+        seg = seg[choice]
+        # point_set = point_set[-self.npoints:, :]
+        # seg = seg[-self.npoints:]
+
+        return point_set, seg
+
+    def __len__(self):
+        return len(self.ids)
+
 
 class ScannetDatasetWholeScene():
     # prepare to give prediction on each points
@@ -116,12 +165,12 @@ class ScannetDatasetWholeScene():
 
     def __getitem__(self, index):
         point_set_ini = self.scene_points_list[index]
-        points = point_set_ini[:,:6]
+        points = point_set_ini[:, :6]
         labels = self.semantic_labels_list[index]
         coord_min, coord_max = np.amin(points, axis=0)[:3], np.amax(points, axis=0)[:3]
         grid_x = int(np.ceil(float(coord_max[0] - coord_min[0] - self.block_size) / self.stride) + 1)
         grid_y = int(np.ceil(float(coord_max[1] - coord_min[1] - self.block_size) / self.stride) + 1)
-        data_room, label_room, sample_weight, index_room = np.array([]), np.array([]), np.array([]),  np.array([])
+        data_room, label_room, sample_weight, index_room = np.array([]), np.array([]), np.array([]), np.array([])
         for index_y in range(0, grid_y):
             for index_x in range(0, grid_x):
                 s_x = coord_min[0] + index_x * self.stride
@@ -132,7 +181,7 @@ class ScannetDatasetWholeScene():
                 s_y = e_y - self.block_size
                 point_idxs = np.where(
                     (points[:, 0] >= s_x - self.padding) & (points[:, 0] <= e_x + self.padding) & (points[:, 1] >= s_y - self.padding) & (
-                                points[:, 1] <= e_y + self.padding))[0]
+                            points[:, 1] <= e_y + self.padding))[0]
                 if point_idxs.size == 0:
                     continue
                 num_batch = int(np.ceil(point_idxs.size / self.block_points))
@@ -166,6 +215,7 @@ class ScannetDatasetWholeScene():
     def __len__(self):
         return len(self.scene_points_list)
 
+
 if __name__ == '__main__':
     data_root = '/data/yxu/PointNonLocal/data/stanford_indoor3d/'
     num_point, test_area, block_size, sample_rate = 4096, 5, 1.0, 0.01
@@ -175,16 +225,21 @@ if __name__ == '__main__':
     print('point data 0 shape:', point_data.__getitem__(0)[0].shape)
     print('point label 0 shape:', point_data.__getitem__(0)[1].shape)
     import torch, time, random
+
     manual_seed = 123
     random.seed(manual_seed)
     np.random.seed(manual_seed)
     torch.manual_seed(manual_seed)
     torch.cuda.manual_seed_all(manual_seed)
+
+
     def worker_init_fn(worker_id):
         random.seed(manual_seed + worker_id)
+
+
     train_loader = torch.utils.data.DataLoader(point_data, batch_size=16, shuffle=True, num_workers=16, pin_memory=True, worker_init_fn=worker_init_fn)
     for idx in range(4):
         end = time.time()
         for i, (input, target) in enumerate(train_loader):
-            print('time: {}/{}--{}'.format(i+1, len(train_loader), time.time() - end))
+            print('time: {}/{}--{}'.format(i + 1, len(train_loader), time.time() - end))
             end = time.time()

data_utils/ShapeNetDataLoader.py
@@ -27,7 +27,6 @@ class PartCustomDataset(Dataset):
         self.ids = np.loadtxt(os.path.join(self.root, split+'.txt')).astype(np.int32)
 
         #todo augmentation
-        foo=0
 
     def __getitem__(self, index):
         point_cloud_path = os.path.join(self.point_clouds_dir,str(self.ids[index])+".txt")
@@ -38,18 +37,17 @@ class PartCustomDataset(Dataset):
             point_set = point_cloud[:, 0:6]
 
         seg_path = os.path.join(self.seg_dir, str(self.ids[index]) + ".txt")
-        seg = np.loadtxt(seg_path).astype(np.int32)
+        seg = np.loadtxt(seg_path).astype(np.int32)-1
 
         point_set[:, 0:3] = pc_normalize(point_set[:, 0:3])
         #TODO maybe normalize 3:6 too? if the colors will be>1
 
         choice = np.random.choice(len(seg), self.npoints, replace=True)
         # resample
-        # point_set = point_set[choice, :]
-        #
-        # seg = seg[choice] #####OF
-        point_set = point_set[-self.npoints:,:]
-        seg = seg[-self.npoints:]
+        point_set = point_set[choice, :]
+        seg = seg[choice]
+        # point_set = point_set[-self.npoints:,:]
+        # seg = seg[-self.npoints:]
 
 
         return point_set, 0, seg

train_partseg.py
@@ -4,7 +4,7 @@ Date: Nov 2019
 """
 import argparse
 import os
-from data_utils.ShapeNetDataLoader import PartCustomDataset as PartNormalDataset ####
+from data_utils.ShapeNetDataLoader import PartCustomDataset
 import torch
 import datetime
 import logging
@@ -20,11 +20,7 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 ROOT_DIR = BASE_DIR
 sys.path.append(os.path.join(ROOT_DIR, 'models'))
 
-seg_classes = {'Earphone': [16, 17, 18], 'Motorbike': [30, 31, 32, 33, 34, 35], 'Rocket': [41, 42, 43], 'Car': [8, 9, 10, 11], 'Laptop': [28, 29], 'Cap': [6, 7], 'Skateboard': [44, 45, 46],
-               'Mug': [36, 37], 'Guitar': [19, 20, 21], 'Bag': [4, 5], 'Lamp': [24, 25, 26, 27], 'Table': [47, 48, 49], 'Airplane': [0, 1, 2, 3], 'Pistol': [38, 39, 40], 'Chair': [12, 13, 14, 15],
-               'Knife': [22, 23]}
-
-seg_classes = {'CustomPart': [0,1,2,3]}
+seg_classes = {'CustomPart': [0,1,2]}
 
 seg_label_to_cat = {}  # {0:Airplane, 1:Airplane, ...49:Table}
 for cat in seg_classes.keys():
@@ -44,7 +40,7 @@ def parse_args():
     parser = argparse.ArgumentParser('Model')
     parser.add_argument('--model', type=str, default='pointnet2_part_seg_msg', help='model name [default: pointnet2_part_seg_msg]')
     parser.add_argument('--batch_size', type=int, default=16, help='Batch Size during training [default: 16]')
-    parser.add_argument('--epoch', default=251, type=int, help='Epoch to run [default: 251]')
+    parser.add_argument('--epoch', default=1000, type=int, help='Epoch to run [default: 251]')
     parser.add_argument('--learning_rate', default=0.001, type=float, help='Initial learning rate [default: 0.001]')
     parser.add_argument('--use_gpu', default="0", type=str, help='If run calcs on gpu')
     parser.add_argument('--gpu', type=str, default='0', help='GPU to use [default: GPU 0]')
@@ -95,18 +91,17 @@ def main(args):
     log_string('PARAMETER ...')
     log_string(args)
 
-    #root = 'data/shapenetcore_partanno_segmentation_benchmark_v0_normal/'
     root = 'data/custom_partseg_data/'
 
 
-    TRAIN_DATASET = PartNormalDataset(root=root, npoints=args.npoint, split='train', normal_channel=args.normal)
+    TRAIN_DATASET = PartCustomDataset(root=root, npoints=args.npoint, split='train', normal_channel=args.normal)
     trainDataLoader = torch.utils.data.DataLoader(TRAIN_DATASET, batch_size=args.batch_size, shuffle=True, num_workers=4)
-    TEST_DATASET = PartNormalDataset(root=root, npoints=args.npoint, split='val', normal_channel=args.normal)
+    TEST_DATASET = PartCustomDataset(root=root, npoints=args.npoint, split='val', normal_channel=args.normal)
     testDataLoader = torch.utils.data.DataLoader(TEST_DATASET, batch_size=args.batch_size, shuffle=False, num_workers=4)
     log_string("The number of training data is: %d" % len(TRAIN_DATASET))
     log_string("The number of test data is: %d" % len(TEST_DATASET))
-    num_classes = 16 #1
-    num_part = 4#50
+    num_classes = 16 #thers'a a bug here, the 16 can't be changed
+    num_part = 3
     '''MODEL LOADING'''
     MODEL = importlib.import_module(args.model)
     shutil.copy('models/%s.py' % args.model, str(experiment_dir))
@@ -179,6 +174,7 @@ def main(args):
         '''learning one epoch'''
         for i, data in tqdm(enumerate(trainDataLoader), total=len(trainDataLoader), smoothing=0.9):
             points, label, target = data
+            B, N, D = points.shape #fixed issue of calc metrics of batch size smaller than planned
             points = points.data.numpy()
             points[:, :, 0:3] = provider.random_scale_point_cloud(points[:, :, 0:3])
             points[:, :, 0:3] = provider.shift_point_cloud(points[:, :, 0:3])
@@ -194,7 +190,7 @@ def main(args):
             target = target.view(-1, 1)[:, 0]
             pred_choice = seg_pred.data.max(1)[1]
             correct = pred_choice.eq(target.data).cpu().sum()
-            mean_correct.append(correct.item() / (args.batch_size * args.npoint))
+            mean_correct.append(correct.item() / (B * args.npoint))
             loss = criterion(seg_pred, target, trans_feat)
             loss.backward()
             optimizer.step()
@@ -293,7 +289,6 @@ def main(args):
         log_string('Best class avg mIOU is: %.5f' % best_class_avg_iou)
         log_string('Best inctance avg mIOU is: %.5f' % best_inctance_avg_iou)
         global_epoch += 1
-    foo=0
 
 
 if __name__ == '__main__':